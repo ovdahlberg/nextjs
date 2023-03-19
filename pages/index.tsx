d1cf4ef81bf550328d57ff31e797cb37d31fb51d
Oliver Dahlberg <oliver@rivercode.se>
2023-03-19 16:08:08 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
-            Get started by staging redeployed!&nbsp;
+            Hello hello!&nbsp;
             <code className={styles.code}>pages/index.tsx</code>
           </p>
           <div>
