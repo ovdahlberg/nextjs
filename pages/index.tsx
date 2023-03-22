d5483d4ba396fa3369acc3513696c706892be14f
Oliver Dahlberg <oliver@rivercode.se>
2023-03-22 13:48:37 +0100

    Updated page

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
-            Get started by hello 2&nbsp;
+            Get started by hello&nbsp;
             <code className={styles.code}>pages/index.tsx</code>
           </p>
           <div>
